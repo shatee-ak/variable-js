4b65882ec2ed7c6329231b416e4d800eb3c5f57b
shatee-ak <abithe99@gmail.com>
2025-01-30 14:10:17 +0600

    solved

---
+++ script.js
@@ -0,0 +1,15 @@
+
+let total=500;
+let kitkat=2;
+let eachprice=40;
+let egg =5;
+let eprice=15;
+ let eggprice = eprice*egg;
+ let kitkatprice = eachprice*kitkat;
+ let totalprice = eggprice+kitkatprice;
+ let remainingmoney= total-totalprice;
+ console.log(remainingmoney);
+
+
+
+
